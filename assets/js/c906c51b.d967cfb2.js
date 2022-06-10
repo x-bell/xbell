4db505955cdae9b3bbe573ae920864120603ba25
xlianghang <xlianghang@163.com>
2022-06-10 11:44:46 +0800

    Updates

---
+++ assets/js/c906c51b.d967cfb2.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[899],{5143:function(e){e.exports=JSON.parse('{"title":"\u88c5\u9970\u5668","slug":"/category/\u88c5\u9970\u5668","permalink":"/xbell/docs/category/\u88c5\u9970\u5668","navigation":{"next":{"title":"\u7528\u4f8b","permalink":"/xbell/docs/api/decorator"}}}')}}]);
