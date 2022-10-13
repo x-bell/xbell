e7661a42d6dd5d2502fb286f22670a163c49c599
xlianghang <xlianghang@163.com>
2022-10-13 19:35:43 +0800

    test: standard-e2e

--- tests/standard-e2e/__tests__/example.test.ts
+++ tests/standard-e2e/__tests__/example.test.ts
@@ -1,6 +1,6 @@
-import { test, expect } from 'xbell';
+import { test } from 'xbell';
 
-test('goto example.com', async ({ page }) => {
+test('goto example.com', async ({ page, expect }) => {
   await page.goto('https://example.com')
   await expect(page).toMatchScreenshot({
     name: 'default-screenshot'
