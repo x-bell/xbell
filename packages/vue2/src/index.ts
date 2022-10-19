8e5ce3bca198f04070934be6def800dd6bf81fb0
xlianghang <xlianghang@163.com>
2022-10-19 15:29:15 +0800

    feat: vue2

--- packages/vue2/src/index.ts
+++ packages/vue2/src/index.ts
@@ -1,32 +1,27 @@
 import { test as basicTest } from 'xbell';
 import type { Component } from 'vue';
 
-const test = basicTest.extendBrowser(async () => {
+export const test = basicTest.extendBrowser(async () => {
   const { default: Vue } = await import('vue');
 
-  function mount<T extends Component>(C: T, props: T extends Component<any, any, any, infer P> ? P : any, rootElement?: HTMLElement | string) {
-    const ele = (() => {
-      if (rootElement) {
-        return typeof rootElement == 'string' ? document.querySelector(rootElement) : rootElement;
-      }
-      return document.getElementById('app') || document.getElementById('root');
-    })();
+  function mount<T extends Component>(C: T, props?: T extends Component<any, any, any, infer P> ? P : any, rootElement?: HTMLElement | string) {
+    const ele = typeof rootElement == 'string'
+      ? document.querySelector(rootElement)
+      : rootElement || document.getElementById('app') || document.getElementById('root');
 
     if (!ele) {
       throw new Error('root must be a element');
     }
     new Vue({
       render(h) {
-        return h(C, props);
+        return h(C, {
+          props,
+        });
       }
-    }).$mount(ele)
+    }).$mount(ele);
   }
 
   return {
     mount,
   }
 });
-
-export {
-  test,
-}
