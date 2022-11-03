c265c4410518d5f21590c1ab8d820fe6fc00da75
xlianghang <xlianghang@163.com>
2022-11-03 19:59:32 +0800

    feat: getter & setter

--- packages/expell/src/spyOn.ts
+++ packages/expell/src/spyOn.ts
@@ -4,69 +4,63 @@ type Func = (...args: any[]) => any
 function isMock(v: any): v is Mock {
   return !!v._isMockFunction;
 }
-function getObjectOriginMethod<T extends object, K extends keyof T>(obj: T, methodName: K,
-  // accessType?: 'get' | 'set'
-): T[K] {
+function getOwnPropertyDescriptor<T extends object, K extends keyof T>(obj: T, methodName: K): PropertyDescriptor | undefined {
   const prototype = Object.getPrototypeOf(obj)
   const descriptor = Object.getOwnPropertyDescriptor(obj, methodName) || Object.getOwnPropertyDescriptor(prototype, methodName);
-  let method = obj[methodName];
-
-  if (!method && descriptor?.get) method = descriptor.get();
-
-  if (isMock(method)) {
-    return method._origin as unknown as T[K];
-  }
-
-  return method as T[K];
+  return descriptor;
 }
 
 function _spyOn<T extends object, K extends keyof T>(
   obj: T,
   methodName: K,
-  // accessType?: 'get' | 'set',
+  accessType: 'get' | 'set' | 'value' = 'value',
   mockFn?: T[K] extends Func ? (...args: Parameters<T[K]>) => ReturnType<T[K]> : Func,
   originFn?: T[K],
 ): T[K] extends Func ? Mock<Parameters<T[K]>, ReturnType<T[K]>> : Mock<any[], any> {
-  // const prototype = Object.getPrototypeOf(obj)
-  // const descriptor = Object.getOwnPropertyDescriptor(obj, methodName) || Object.getOwnPropertyDescriptor(prototype, methodName);
-  let origin: T[K] | undefined = originFn;
-  // if (!accessType) {
-  if (!origin) origin = getObjectOriginMethod(obj, methodName);
-    // @ts-ignore
-  obj[methodName] = spy(mockFn ?? origin.bind?.(obj)) as Mock;
-  // }
-  // else {
-  //   if (descriptor?.[accessType]) {
-  //     if (!origin) origin = getObjectOriginMethod(obj, methodName, accessType);
-  //     // @ts-ignore
-  //     descriptor[accessType] = spy(mockFn ?? origin.bind(obj) as (...args: any[]) => any);
-  //     Object.defineProperty(obj, methodName, descriptor);
-  //   }
-  // }
+  const descriptor = getOwnPropertyDescriptor(obj, methodName);
+  const origin = originFn ?? (() => {
+    if (isMock(obj[methodName])) {
+      return obj[methodName];
+    }
 
-  const mock = obj[methodName] as unknown as Mock;
+    if (descriptor) {
+      // 1. { key() {} }
+      // 2. get key () { return xx }
+      // 3. set key() {}
+      return descriptor[accessType];
+    }
+    
+    return accessType === 'value' ? obj[methodName] : () => obj[methodName];
+
+  })();
+  const mock = spy(mockFn ?? origin.bind?.(obj)) as Mock;
+
+  Object.defineProperty(obj, methodName, {
+    ...descriptor,
+    [accessType]: mock,
+  });
 
   mock.mockImplementation = (impl: T[K] extends Func ? (...args: Parameters<T[K]>) => ReturnType<T[K]> : Func) => {
-    return _spyOn(obj, methodName, impl, origin);
+    return _spyOn(obj, methodName, accessType, impl, origin);
   };
 
   mock.mockReturnValue = ((v: T[K] extends Func ? ReturnType<T[K]> : any) => {
-    return _spyOn(obj, methodName, () => v, origin);
+    return _spyOn(obj, methodName, accessType, () => v, origin);
   });
 
   // @ts-ignore
-  return obj[methodName];
+  return mock;
 }
 
 // type A = 
 export function spyOn<T extends object, K extends keyof T>(
   object: T,
   methodName: K,
-  // accessType?: 'get' | 'set',
+  accessType?: 'get' | 'set',
 ): T[K] extends Func ? Mock<Parameters<T[K]>, ReturnType<T[K]>> : Mock<any[], any> {
   const original = object[methodName];
   if (isMock(original)) {
     return object[methodName] as any;
   }
-  return _spyOn(object, methodName);
+  return _spyOn(object, methodName, accessType);
 }
