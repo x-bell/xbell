96d579603a15c35d628b1c737df1763544f93e5c
xlianghang <xlianghang@163.com>
2022-11-16 21:18:52 +0800

    test: e2e

--- tests/standard-e2e/src/example.test.ts
+++ tests/standard-e2e/src/example.test.ts
@@ -1,7 +1,10 @@
 import { test } from 'xbell';
 
 test('goto example.com', async ({ page, expect }) => {
-  await page.goto('https://example.com')
+  await page.goto('https://example.com');
+  // no error
+  await page.get('.xx').first().getByClass('xx');
+  // await page.get('.xx').first().getByClass('xx').click();
   await expect(page).toMatchScreenshot({
     name: 'default-screenshot'
   });
