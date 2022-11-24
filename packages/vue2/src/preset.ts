f95e974c0aec14d51be6bd60385107304a70bdae
xlianghang <xlianghang@163.com>
2022-11-24 23:25:46 +0800

    feat: vue2

--- packages/vue2/src/preset.ts
+++ packages/vue2/src/preset.ts
@@ -1,10 +1,9 @@
 import type { XBellConfig } from 'xbell';
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue2'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue2';
 
 export const preset: XBellConfig = {
   browser: {
-    headless: false,
     devServer: {
       viteConfig: defineConfig({
         plugins: [vue()],
@@ -14,12 +13,6 @@ export const preset: XBellConfig = {
   browserTest: {
     html: {
       content: '<div id="app"></div>'
-    }
-  },
-  compiler: {
-    jsx: {
-      importSource: '@lancercomet/vue2-jsx-runtime',
-      runtime: 'automatic',
     }
   },
 };
