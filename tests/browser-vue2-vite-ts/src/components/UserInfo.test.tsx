22358e70c348fb5cb5fc5f8aa76a36a8a63659a8
xlianghang <xlianghang@163.com>
2022-11-25 10:51:46 +0800

    test: vue2

--- tests/browser-vue2-vite-ts/src/components/UserInfo.test.tsx
+++ tests/browser-vue2-vite-ts/src/components/UserInfo.test.tsx
@@ -1,16 +1,12 @@
 import { test } from '@xbell/vue2';
 
 test.browser('render', async ({ page, expect, mount }) => {
-  const { defineComponent } = await import('vue');
   const { default: UserInfo } = await import('./UserInfo.vue');
-  mount(defineComponent({
-    setup() {
-      return () => (
-        <UserInfo username="xlianghang" />
-      )
+  mount(UserInfo, {
+    props: {
+      username: 'xlianghang',
     }
-  }));
-
+  });
   await page.waitForRequestIdle();
   await expect(page.getByText('Hang Liang')).toBeVisible();
   await expect(page).toMatchScreenshot({
