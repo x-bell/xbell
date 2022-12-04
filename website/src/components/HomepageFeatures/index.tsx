86a7dfdab95e6771b1c059e3b0438f111eac910d
xlianghang <xlianghang@163.com>
2022-12-04 22:27:36 +0800

    docs: website

--- website/src/components/HomepageFeatures/index.tsx
+++ website/src/components/HomepageFeatures/index.tsx
@@ -2,40 +2,72 @@ import React from 'react';
 import clsx from 'clsx';
 import Translate from '@docusaurus/Translate';
 import styles from './styles.module.css';
-
+import lottieWeb from 'lottie-web';
+import type { AnimationItem } from 'lottie-web';
 type FeatureItem = {
   title: string;
-  src: string;
+  // src: string;
+  data: any;
   description: string;
+  scale?: number;
+  speed?: number;
 };
 
 const FeatureList: FeatureItem[] = [
   {
+    title: 'homepage.realRuntime',
+    data: 'https://raw.githubusercontent.com/x-bell/xbell-assets/main/lottie/runtime.json',
+    description: 'homepage.realRuntime.desc',
+    speed: 0.5
+  },
+  {
+    title: 'homepage.snapshots',
+    data: 'https://raw.githubusercontent.com/x-bell/xbell-assets/main/lottie/camera.json',
+    description: 'homepage.snapshots.desc',
+  },
+  {
     title: 'homepage.allInOneTesting',
-    src: 'https://raw.githubusercontent.com/x-bell/xbell-assets/main/homepage/xbell-mods.svg',
+    data: 'https://raw.githubusercontent.com/x-bell/xbell-assets/main/lottie/all-in-one.json',
     description: 'homepage.allInOneTesting.desc',
   },
   {
-    title: 'homepage.nodeAndBrowser',
-    src: 'https://raw.githubusercontent.com/x-bell/xbell-assets/main/homepage/xbell-cooperation.svg',
-    description: 'homepage.nodeAndBrowser.desc',
-  },
-  {
-    title: 'homepage.decoratorMode',
-    src: 'https://raw.githubusercontent.com/x-bell/xbell-assets/main/homepage/xbell-decorators.svg',
-    description: 'homepage.decoratorMode.desc',
+    title: 'homepage.outOfBox',
+    data: 'https://raw.githubusercontent.com/x-bell/xbell-assets/main/lottie/out-of-box.json',
+    description: 'homepage.outOfBox.desc',
+    scale: 1.6,
   },
 ];
 
-function Feature({title, src, description}: FeatureItem) {
+function Feature({title, data, scale, description, speed}: FeatureItem) {
+  const iconRef = React.useRef<HTMLDivElement>();
+  React.useLayoutEffect(() => {
+    if (!data) return;
+    const lottieItem = lottieWeb.loadAnimation({
+      renderer: 'svg',
+      path: data,
+      loop: 5,
+      autoplay: true,
+      container: iconRef.current,
+    });
+
+    if (speed) {
+      lottieItem.setSpeed(speed)
+    }
+
+    return () => {
+      lottieItem.destroy();
+    }
+  }, []);
+
   return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
+    <div className={clsx('col col--3')}>
+      {/* <div className="text--center">
         <img className={styles.featureSvg} role="img" src={src} />
-      </div>
+      </div> */}
       <div className="text--center padding-horiz--md">
+        <div ref={iconRef} style={{ width: 30, height: 30, display: 'inline-block', transform: `scale(${scale})`, }} />
         <h3><Translate id={title} /></h3>
-        <p><Translate id={description} /></p>
+        <p style={{ color: '#666' }}><Translate id={description} /></p>
       </div>
     </div>
   );
--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -5,20 +5,62 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Translate, {translate} from '@docusaurus/Translate';
-
-
-
+import lottieWeb from 'lottie-web';
 import styles from './index.module.css';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
+  const rightRef = React.useRef<HTMLDivElement>();
+  const getStartedRef = React.useRef<HTMLDivElement>();
+  React.useLayoutEffect(() => {
+    const automationLottieItem = lottieWeb.loadAnimation({
+      path: 'https://raw.githubusercontent.com/x-bell/xbell-assets/main/lottie/automation.json',
+      container: rightRef.current,
+      loop: true,
+      autoplay: true,
+      renderer: 'svg'
+    });
+    const bellLottieItem = lottieWeb.loadAnimation({
+      path: 'https://raw.githubusercontent.com/x-bell/xbell-assets/main/lottie/bell.json',
+      container: getStartedRef.current,
+      loop: 1,
+      autoplay: true,
+      renderer: 'svg',
+    });
+    return () => {
+      automationLottieItem.destroy();
+      bellLottieItem.destroy();
+    }
+  }, [])
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-       <div className={styles.fullLogoContainer}>
+    <header className={clsx(styles.heroBanner)}>
+       {/* <div className={styles.fullLogoContainer}>
           <img src="https://raw.githubusercontent.com/x-bell/xbell-assets/main/logo/xbell-512.svg" width={250} height={250} />
+        </div> */}
+      <div className={clsx(styles.container)}>
+        <div className={styles.containerLeft}>
+          <div className={styles.title}>
+            {siteConfig.title}
+          </div>
+          <div className={styles.slogan}>
+            Make Testing Easy
+          </div>
+          <div className={styles.description}>
+            Let all the code run in a real environment for testing!
+          </div>
+          <div className={styles.buttons}>
+            <Link
+              className={clsx('button button--secondary button--lg', styles.getStarted)}
+              to="/docs/get-started">
+                <Translate id="homepage.getStarted" />
+                <div style={{ display: 'inline-block', width: 30, height: 30, marginLeft: 4, }} ref={getStartedRef}></div>
+            </Link>
+          </div> 
         </div>
-      <div className={clsx('container', styles.container)}>
-        <h1 className="hero__title">{siteConfig.title}</h1>
+        <div className={styles.containerRight}>
+          <div ref={rightRef} style={{ width: 350, height: 300 }} />
+        </div>
+        {/* <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle"><Translate id="xbell.slogan" /></p>
         <div className={styles.buttons}>
           <Link
@@ -26,7 +68,7 @@ function HomepageHeader() {
             to="/docs/get-started">
               <Translate id="homepage.getStarted" />
           </Link>
-        </div>
+        </div> */}
       </div>
     </header>
   );
