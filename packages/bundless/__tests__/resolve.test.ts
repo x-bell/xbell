15a6c475a4a42f261497093d8d8a325fdf8ab4c8
xlianghang <xlianghang@163.com>
2022-12-10 13:22:53 +0800

    feat: resovle

--- packages/bundless/__tests__/resolve.test.ts
+++ packages/bundless/__tests__/resolve.test.ts
@@ -1,12 +1,21 @@
 import { test } from 'xbell';
+import * as path from 'node:path';
 import { createRequire } from 'node:module';
 import { resolve } from '../src/resolve';
 import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(new URL(import.meta.url));
+const __dirname = path.dirname(__filename);
+
+
+const mainTSFilename = path.join(
+  __dirname,
+  './fixtures/src/main.ts'
+);
+
 const require = createRequire(import.meta.url);
 
-test('#resolve', async ({ expect }) => {
+test('#resolve - pkg', async ({ expect }) => {
   const { filename } = resolve({
     importer: __filename,
     specifier: '@swc/core'
@@ -14,4 +23,14 @@ test('#resolve', async ({ expect }) => {
 
   const retByNative = require.resolve('@swc/core');
   console.log('ret', filename, retByNative);
+});
+
+test('#resolve - file', ({ expect }) => {
+  console.log('mainTSFilename', mainTSFilename);
+  const { filename } = resolve({
+    importer: mainTSFilename,
+    specifier: './add',
+  });
+  const relativeFilename = path.relative(__dirname, filename);
+  expect(relativeFilename).toBe('fixtures/src/add.ts');
 });
